0bc87e27886830a638909f9982d9f6f8107a4fec
nomanaa <48934180+nomanaa@users.noreply.github.com>
2020-02-15 12:06:09 -0600

    Update Sprite.java

--- Sprite.java
+++ Sprite.java
@@ -1,12 +1,4 @@
-/**
-  *[ Sprite ]
-  *COMP1020 SECTION[A02]
-  *INSTRUCTOR:[ Olivier Tremblay-Savard ]
-  *NAME:[ Abdullah Al Noman]
-  *ASSIGNMENT:[Assignment 4]
-  *QUESTION:[ All of the parts of the assignement is completed except the bonus part]
-  **PURPOSE:[ This is an abstract class which has dummy methods]
-  */
+
 import java.awt.Color;
 
 public abstract class Sprite {
